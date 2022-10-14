bc5f272156332f755e6f88e3e199972cc10b5466
ADITYA290802 <adipatel016@gmail.com>
2022-10-14 23:09:10 +0530

    practical-6

---
+++ app/src/main/java/com/example/prac_6_076/youtubeactivity.kt
@@ -0,0 +1,12 @@
+package com.example.prac_6_076
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class youtubeactivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_youtubeactivity)
+
+    }
+}
